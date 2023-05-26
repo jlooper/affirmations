8aa3612cc7d7872c6b44a08f9b105e807609e970
jlooper <jen.looper@gmail.com>
2023-05-25 22:45:57 -0400

    no ssr on gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-  output: 'server'
+  integrations: [preact()]
 });
