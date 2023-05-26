d46d4bdad474f6d59a11a762c0613c1832908b6b
jlooper <jen.looper@gmail.com>
2023-05-25 23:39:40 -0400

    jsx edits

--- src/components/AffirmationComponent.jsx
+++ src/components/AffirmationComponent.jsx
@@ -1,11 +1,37 @@
+const data = await fetch('https://e51pzppdu1.execute-api.us-east-1.amazonaws.com/items').then((response) =>
+    response.json()
+);
+
+console.log(data)
+
+const Affirmation = () => {
+  return <h3>{JSON.stringify(data[0].Text)}</h3>;
+};
+      
+export default Affirmation;
+
+
+/*import { h } from 'preact';
 import { useState } from 'preact/hooks';
 
-export default function Affirmation({affirmations}) {
+const messages = await fetch('https://e51pzppdu1.execute-api.us-east-1.amazonaws.com/items').then((response) =>
+    response.json()
+);
 
-  const num = Math.floor(Math.random() * affirmations.length);
-  const [affirmation] = useState(JSON.stringify(affirmations[num].Text));
+console.log(messages)
+
+export default function Greeting() {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+  
+  const [affirmation, setAffirmation] = useState(messages[0]);
 
   return (
-      <h3>{affirmation}</h3>
+    <div> 
+      <h3>{affirmation}! Thank you for visiting!</h3>
+      <button onClick={() => setAffirmation(randomMessage())}>
+        New Affirmation
+      </button>
+    </div>
   );
-}
+}*/
