f1ffb8e8e02e084e025d5882bc4d19f231dd350b
jlooper <jen.looper@gmail.com>
2023-05-25 23:46:35 -0400

    cleanup - cors issues

--- src/components/AffirmationComponent.jsx
+++ src/components/AffirmationComponent.jsx
@@ -2,36 +2,8 @@ const data = await fetch('https://e51pzppdu1.execute-api.us-east-1.amazonaws.com
     response.json()
 );
 
-console.log(data)
-
 const Affirmation = () => {
   return <h3>{JSON.stringify(data[0].Text)}</h3>;
 };
       
 export default Affirmation;
-
-
-/*import { h } from 'preact';
-import { useState } from 'preact/hooks';
-
-const messages = await fetch('https://e51pzppdu1.execute-api.us-east-1.amazonaws.com/items').then((response) =>
-    response.json()
-);
-
-console.log(messages)
-
-export default function Greeting() {
-
-  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-  
-  const [affirmation, setAffirmation] = useState(messages[0]);
-
-  return (
-    <div> 
-      <h3>{affirmation}! Thank you for visiting!</h3>
-      <button onClick={() => setAffirmation(randomMessage())}>
-        New Affirmation
-      </button>
-    </div>
-  );
-}*/
